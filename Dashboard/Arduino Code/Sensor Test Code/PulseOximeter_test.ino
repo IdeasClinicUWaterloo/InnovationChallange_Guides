4e74261ef6f6c36b98bace497f28008c83cb6ec6
Jack Gordon <72707894+jgordon314@users.noreply.github.com>
2025-01-15 14:31:37 -0500

    Update PulseOximeter_test.ino

--- Dashboard/Arduino Code/Sensor Test Code/PulseOximeter_test.ino
+++ Dashboard/Arduino Code/Sensor Test Code/PulseOximeter_test.ino
@@ -12,24 +12,29 @@ DFRobot_MAX30102 particleSensor;
 void setup() {
     Serial.begin(115200);
 
-  if (!particleSensor.begin()) {
-    Serial.println("MAX30102 was not found");
+  while (!particleSensor.begin()) {
+    Serial.println("MAX30102 was not found, check your wiring!");
+    delay(100);
   }
 
-  particleSensor.sensorConfiguration(/*ledBrightness=*/150, /*sampleAverage=*/SAMPLEAVG_8,
-                                     /*ledMode=*/MODE_MULTILED, /*sampleRate=*/SAMPLERATE_200,
+  particleSensor.sensorConfiguration(/*ledBrightness=*/50, /*sampleAverage=*/SAMPLEAVG_16,
+                                     /*ledMode=*/MODE_MULTILED, /*sampleRate=*/SAMPLERATE_800,
                                      /*pulseWidth=*/PULSEWIDTH_411, /*adcRange=*/ADCRANGE_16384);
-}
-
-void calculateHR() {
-    particleSensor.heartrateAndOxygenSaturation(/**SPO2=*/&SPO2, /**SPO2Valid=*/&SPO2Valid, /**heartRate=*/&heartRate, /**heartRateValid=*/&heartRateValid);
 }
 
 void loop()
 {
-  Serial.print("HR detected: "); Serial.println(heartRateValid); // 1=True, 0=False
-  Serial.print("HR: "); Serial.println(heartRate);
-  Serial.print("SPO2 detected: "); Serial.println(SPO2Valid); // 1=True, 0=False
-  Serial.print("SPO2: "); Serial.println(SPO2);
+  particleSensor.heartrateAndOxygenSaturation(/**SPO2=*/&SPO2, /**SPO2Valid=*/&SPO2Valid, /**heartRate=*/&heartRate, /**heartRateValid=*/&heartRateValid);
+  
+  if (heartRateValid == 1){
+    Serial.print("Detected Heart Rate: "); Serial.println(heartRate);
+  }else{
+    Serial.println("No heart rate detected.");
+  }
+  if (SPO2Valid == 1){
+    Serial.print("Detected Blood Oxygenation %: "); Serial.println(SPO2);
+  }else{
+    Serial.println("No blood oxygenation levels detected.");
+  }
   delay(100);
 }
