4b4f367f191157d8503dc93de521de78b7b8d533
IdeasClinicUWaterloo <ideasclinictic@gmail.com>
2025-01-30 14:46:16 -0500

    Update untrain.py

--- AI_Companion/Chatbot_code/untrain.py
+++ AI_Companion/Chatbot_code/untrain.py
@@ -1,6 +1,6 @@
 '''
-Run This code to clear all data previously learned from training lessons. The bot will store the chats in the "db.sqlite3" file
-even after the program has ended, so if undesired topics/ words are taught this will clear everythig and begin the training from
+Run this code to clear all data previously learned from training lessons. The bot will store the chats in the "db.sqlite3" file
+even after the program has ended, so if undesired topics/ words are taught this will clear everything and begin the training from
 the source code again.
 '''
 
